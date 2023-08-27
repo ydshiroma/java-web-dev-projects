fc5cb51e8eea314c712382ba3df0a14ba2f80ba3
Yumi <ydshiroma@gmail.com>
2023-08-27 17:25:12 -0400

    bonus missions

--- control-flow-and-collections/studio/counting-characters/src/main/java/org/launchcode/CaseInsensitive.java
+++ control-flow-and-collections/studio/counting-characters/src/main/java/org/launchcode/CaseInsensitive.java
@@ -1,13 +1,16 @@
 package org.launchcode;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
-public class CharacterCount {
+public class CaseInsensitive {
     public static void main(String[] args) {
-        String str = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter some text: ");
+        String strCaseSensitive = input.nextLine();
+        input.close();
+
+        String str = strCaseSensitive.toLowerCase();
+
         char[] charactersInString = str.toCharArray();
 //        System.out.println(charactersInString[0]);
 
--- control-flow-and-collections/studio/counting-characters/src/main/java/org/launchcode/CharacterCountUserInput.java
+++ control-flow-and-collections/studio/counting-characters/src/main/java/org/launchcode/CharacterCountUserInput.java
@@ -1,13 +1,14 @@
 package org.launchcode;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
-public class CharacterCount {
+public class CharacterCountUserInput {
     public static void main(String[] args) {
-        String str = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter some text: ");
+        String str = input.nextLine();
+        input.close();
+
         char[] charactersInString = str.toCharArray();
 //        System.out.println(charactersInString[0]);
 
